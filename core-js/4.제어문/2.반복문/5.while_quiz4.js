aefb11cffd90f6a7ed816344c576d6afe1314ec9
Kangbcgit <Kangbcgit@gmail.com>
2021-11-29 14:22:27 +0900

    home

--- core-js/4.제어문/2.반복문/5.while_quiz4.js
+++ core-js/4.제어문/2.반복문/5.while_quiz4.js
@@ -1,16 +1,29 @@
-var last = +prompt(`양의 정수를 입력해주세요.`),
-    n = 1,
-    ten = 0,
-    text = ``;
+// var last = +prompt(`양의 정수를 입력해주세요.`),
+//     n = 1,
+//     ten = 0,
+//     text = ``;
 
-while (n <= last) {
-    if (ten <10) {
-        text +=`${ten + 1}`;
-        ten++;
-    }else {
-        ten -= 10;
-        text +=`${ten}`;
-    }
+// while (n <= last) {
+//     if (ten <10) {
+//         text +=`${ten + 1}`;
+//         ten++;
+//     }else {
+//         ten -= 10;
+//         text +=`${ten}`;
+//     }
+//     n++;
+// }
+// alert(text);
+
+
+var max = +prompt(`양의 정수를 입력해주세요`),
+    text = ``,
+    n = 1;
+
+
+while (n <= max) {
+    text += `${n % 10}`
     n++;
 }
+
 alert(text);
