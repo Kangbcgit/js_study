f8d8b068d38a38db361bc054c31d45e318e42f5a
Kangbcgit <Kangbcgit@gmail.com>
2021-12-02 13:04:06 +0900

    [21.12.02]#home

---
+++ core-js/4.제어문/3.탈출문/9.hquiz.js
@@ -0,0 +1,59 @@
+/* quiz 1
+// 유저 입력 부분.
+var userInput = +prompt(`넓이: `);
+
+// 반복적으로 약수를 추출
+var i = 1,
+    num = ``
+while (i <= userInput) {
+    if (userInput % i === 0) {
+        if (i < userInput / i) {
+            num += `${i} x ${userInput / i}\n`;
+        }
+    }
+    i++;
+}
+alert(num)
+*/
+
+
+/* quiz2
+var userInputStar = +prompt(`몇 개의 *를 표시할까요?`),
+    userInputEnter = +prompt(`몇 개마다 줄 바꿈할까요?`);
+
+var i = 0,
+    j = 0,
+    star = ``;
+while (i < userInputStar) {
+    if (++j % userInputEnter === 0) {
+        star += `*\n`
+    } else {
+        star += `*`
+    }
+    i++;
+}
+
+alert(star);
+*/
+
+class Reader
+{
+    constructor(__input)
+    {
+        this.__input = __input;
+        this.__index = 0;
+    }
+
+    read() {
+        const output = this.__input[this.__index++];
+        return output;
+    }
+}
+
+const __input = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(v => Number(v));
+var __reader = new Reader(__input);
+
+var a = Math.floor(Math.random() * 5) + 100,
+    b = Math.floor(Math.random() * 5) + 100;
+
+
