622d6270047cf34125e990e4aaeaf0e4f706f0e7
Kangbcgit <Kangbcgit@gmail.com>
2021-12-07 18:47:59 +0900

    .

--- core-js/7.객체/5.quiz02.js
+++ core-js/7.객체/5.quiz02.js
@@ -7,12 +7,12 @@ while (true) {
 var id = prompt(`아이디를 입력하세요`);
     if (id in user) {
         if (user[id] === prompt('비밀번호를 입력해주세요.')) {
-            alert('로그인에 성공하셨습니다.')
+            alert('로그인에 성공하셨습니다.');
             break;
         } else {
-            alert('비밀번호가 틀렸습니다.')
+            alert('비밀번호가 틀렸습니다.');
         }
     } else {
-        alert('존재하지 않는 회원입니다.')
+        alert('존재하지 않는 회원입니다.');
     }
 }
